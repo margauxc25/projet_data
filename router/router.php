6e415e4486accf1a54755c691499a9948888f0f1
Poupeney Clément <clement.poupeney.monnin@gmail.com>
2021-08-09 15:56:32 +0200

    MAJ structure mvc

--- router/router.php
+++ router/router.php
@@ -5,21 +5,22 @@ require 'vendor/autoload.php';
 $router = new AltoRouter();
 $router->setBasePath('projet_data/');
 
+
+
 $router->map('GET', '/', function() {
-require 'testhome.php';
+require './controller/index_controller.php';
 });
 
-$router->map('GET', '/aaa', function() {
-require 'testaaa.php';
+$router->map('GET', '/infos', function() {
+require './controller/infos_controller.php';
 });
-
 
 $match = $router->match();
 if($match !== null){
 if (is_callable($match['target'])){
 call_user_func($match['target']);
 } else {
-require 'test404.php';
+require './controller/404_controller.php';
 }
 } else {
 echo'error';
