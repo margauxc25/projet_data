a00adaf674b9ffa7a48e2e4033004a546bf59948
Poupeney Clément <clement.poupeney.monnin@gmail.com>
2021-08-10 15:06:20 +0200

    maj requetes

--- requests.php
+++ requests.php
@@ -1,11 +1,36 @@
 <?php
 
+// CONNECT TO DATABASE - CHANGE SETTINGS TO YOUR OWN!
+$dbHost = 'localhost';
+$dbName = 'mydb';
+$dbChar = 'utf8';
+$dbUser = 'root';
+$dbPass = 'Wormhole.9';
+try {
+  $pdo = new PDO(
+    "mysql:host=".$dbHost.";dbname=".$dbName.";charset=".$dbChar,
+    $dbUser, $dbPass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
+} catch (Exception $ex) { exit($ex->getMessage()); }
+
+
 
 // INDEX REQUEST, ALL POKEMONS WITH ID NAME & URL
 
-// SELECT infos.id_pokemon, infos.name, infos.url FROM infos
+
+$index_card = $pdo->prepare(
+    "SELECT infos.id_pokemon, infos.name, infos.url FROM infos"
+);
 
 
 
 // INDEX REQUEST BY TYPE
+
+
+
+
+
+// INDEX REQUEST BY GENERATIONS
+
+
 
