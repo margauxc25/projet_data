dbb1e40424e1c08bf56a9074b43c5112c9dd36c4
Poupeney Clément <clement.poupeney.monnin@gmail.com>
2021-08-18 20:58:17 +0200

    Last request

--- requests.php
+++ requests.php
@@ -5,7 +5,7 @@ $dbHost = 'localhost';
 $dbName = 'mydb';
 $dbChar = 'utf8';
 $dbUser = 'root';
-$dbPass = 'Wormhole.9';
+$dbPass = 'MDP';
 try {
   $pdo = new PDO(
     "mysql:host=".$dbHost.";dbname=".$dbName.";charset=".$dbChar,
@@ -27,38 +27,131 @@ print_r($all);
 
 // INDEX REQUEST BY TYPE
 
-SELECT infos.id_pokemon, infos.name, infos.url
-FROM types_has_infos
-JOIN types
-ON types_has_infos.types_id_type = types.id_type
-JOIN infos
-ON types_has_infos.infos_id_pokemon = infos.id_pokemon
-WHERE types.type = "Grass"
+// SELECT infos.id_pokemon, infos.name, infos.url
+// FROM types_has_infos
+// JOIN types
+// ON types_has_infos.types_id_type = types.id_type
+// JOIN infos
+// ON types_has_infos.infos_id_pokemon = infos.id_pokemon
+// WHERE types.type = "Grass"
 
 
 
 // INDEX REQUEST BY GENERATIONS
 
-SELECT infos.id_pokemon, infos.name, infos.url//, generations.generation
-FROM infos
-JOIN generations
-ON infos.generations_id_generations = generations.id_generations
-WHERE infos.generations_id_generations = 1
+// SELECT infos.id_pokemon, infos.name, infos.url//, generations.generation
+// FROM infos
+// JOIN generations
+// ON infos.generations_id_generations = generations.id_generations
+// WHERE infos.generations_id_generations = 1
 
 
 // INFOS POKEMON
 
-SELECT infos.id_pokemon, infos.name, infos.url, types.type, generations.generation, infos.total, infos.hp, infos.attack, infos.defense, infos.special_attack, infos.special_defense, infos.speed
-FROM infos
-JOIN types_has_infos
-ON types_has_infos.infos_id_pokemon = infos.id_pokemon
-JOIN types
-ON types_has_infos.types_id_type = types.id_type
-JOIN generations
-ON infos.generations_id_generations = generations.id_generations
-JOIN stats
-ON
-WHERE infos.id_pokemon = 151
+// CREATE VIEWS
+
+// TOTAL
+
+// CREATE VIEW total AS
+// SELECT infos.id_pokemon, stats.value AS total
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.total
+
+// HP
+
+// CREATE VIEW hp AS
+// SELECT infos.id_pokemon, stats.value AS hp
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.hp
+
+// ATTACK
+
+// CREATE VIEW attack AS
+// SELECT infos.id_pokemon, stats.value AS attack
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.attack
+
+
+// DEFENSE
+
+// CREATE VIEW defense AS
+// SELECT infos.id_pokemon, stats.value AS defense
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.defense
+
+
+// SPECIAL_ATTACK
+
+// CREATE VIEW special_attack AS
+// SELECT infos.id_pokemon, stats.value AS special_attack
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.special_attack
+
+
+// PUTAIN DE REQUETE
+
+// SELECT infos.id_pokemon, infos.name, infos.url, types.type, generations.generation, total.total, hp.hp, attack.attack, defense.defense, special_attack.special_attack, special_defense.special_defense, speed.speed
+// FROM infos
+// JOIN types_has_infos
+// ON types_has_infos.infos_id_pokemon = infos.id_pokemon
+// JOIN types
+// ON types_has_infos.types_id_type = types.id_type
+// JOIN generations
+// ON infos.generations_id_generations = generations.id_generations
+// JOIN total
+// ON total.id_pokemon = infos.id_pokemon
+// JOIN hp
+// ON hp.id_pokemon = infos.id_pokemon
+// JOIN attack
+// ON attack.id_pokemon = infos.id_pokemon
+// JOIN defense
+// ON defense.id_pokemon = infos.id_pokemon
+// JOIN special_attack
+// ON special_attack.id_pokemon = infos.id_pokemon
+// JOIN special_defense
+// ON special_defense.id_pokemon = infos.id_pokemon
+// JOIN speed
+// ON speed.id_pokemon = infos.id_pokemon
+// WHERE infos.id_pokemon = 897
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TEEEEEEEEEEEEEEEEEEEEEEST
+
+
+// SELECT infos.id_pokemon, infos.name, infos.url, types.type, generations.generation, infos.total, infos.hp, infos.attack, infos.defense, infos.special_attack, infos.special_defense, infos.speed
+// FROM infos
+// JOIN types_has_infos
+// ON types_has_infos.infos_id_pokemon = infos.id_pokemon
+// JOIN types
+// ON types_has_infos.types_id_type = types.id_type
+// JOIN generations
+// ON infos.generations_id_generations = generations.id_generations
+// JOIN stats
+// ON
+// WHERE infos.id_pokemon = 151
 
 
 
@@ -66,26 +159,16 @@ WHERE infos.id_pokemon = 151
 
 // TEST 
 
-SELECT stats.value AS hp, stats.value AS total
-FROM stats
-INNER JOIN infos
-ON infos.hp = stats.id_stats
-WHERE infos.id_pokemon = 151
+// SELECT stats.value AS hp, stats.value AS total
+// FROM stats
+// INNER JOIN infos
+// ON infos.hp = stats.id_stats
+// WHERE infos.id_pokemon = 151
 
 
 
-
-
-
-
-
-
-
-
-
-
-SELECT infos.id_pokemon, infos.name, infos.url, stats.value
-FROM infos
-JOIN stats
-ON stats.id_stats = infos.total
-WHERE infos.id_pokemon = 151
+// SELECT infos.id_pokemon, infos.name, infos.url, stats.value
+// FROM infos
+// JOIN stats
+// ON stats.id_stats = infos.total
+// WHERE infos.id_pokemon = 151
