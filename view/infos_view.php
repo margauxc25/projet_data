8494eca05691e22ad0bcdb76b9aff74b17b8aa20
Poupeney Clément <clement.poupeney.monnin@gmail.com>
2021-08-24 15:41:15 +0200

    maj

--- view/infos_view.php
+++ view/infos_view.php
@@ -11,6 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/statsstyle.css">
+        <link rel="stylesheet" href="./assets/css/footerstyle.css">
+
     <title>Pokedata</title>
 </head>
 
